31da1810c2f426e73b4c4d0cee7f84207afc0c9d
corpzbrideee02 <59422083+corpzbrideee02@users.noreply.github.com>
2020-01-15 21:53:17 -0500

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,86 @@
+/* 
+prog name: Canadian flag in java
+ */
+
+public class Pattern4
+{
+	public static void main(String[]args)
+	{
+		
+		
+		//1 
+		System.out.println("\t__________________________________________________");
+		//2
+		System.out.println("\t|            |                       |            |");
+		//3
+		System.out.println("\t|            |                       |            |");
+		//4
+		System.out.println("\t|            |                       |            |");
+		//5
+		System.out.println("\t|            |           *           |            |");
+		//6
+		System.out.println("\t|            |          ***          |            |");
+		//7
+		System.out.println("\t|            |         *****         |            |");
+		//8
+		System.out.println("\t|            |      ***********      |            |");
+		//9
+		System.out.println("\t|            |      ***********      |            |");
+		//10
+		System.out.println("\t|            |      ***********      |            |");
+		
+		//______________________________
+		//11 
+		System.out.println("\t|            |       *********       |            |");
+		//12
+		System.out.println("\t|            |       *********       |            |");
+		//13
+		System.out.println("\t|            |   **  *********  **   |            |");
+		//14
+		System.out.println("\t|            |** ***  *******  *** **|            |");
+		//15
+		System.out.println("\t|            |******* ******* *******|            |");
+		//16
+		System.out.println("\t|            |***********************|            |");
+		//17
+		System.out.println("\t|            | ********************* |            |");
+		//18
+		System.out.println("\t|            |***********************|            |");
+		//19
+		System.out.println("\t|            | ********************* |            |");
+		//20
+		System.out.println("\t|            |  *******************  |            |");
+		
+		//______________________________
+		//21 
+		System.out.println("\t|            |   *****************   |            |");
+		//22
+		System.out.println("\t|            |    ***************    |            |");
+		//23
+		System.out.println("\t|            |     *************     |            |");
+		//24
+		System.out.println("\t|            |       *********       |            |");
+		//25
+		System.out.println("\t|            |      ***********      |            |");
+		//26
+		System.out.println("\t|            |           *           |            |");
+		//27
+		System.out.println("\t|            |           *           |            |");
+		//28
+		System.out.println("\t|            |           *           |            |");
+		//29
+		System.out.println("\t|            |           *           |            |");
+		//30
+		System.out.println("\t|            |           *           |            |");
+		
+		//31
+		System.out.println("\t|            |                       |            |");
+		//32
+		System.out.println("\t__________________________________________________");
+		
+		
+		
+		
+	}//end main
+	
+}//END OF CLASS
